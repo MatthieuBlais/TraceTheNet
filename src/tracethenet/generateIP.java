15dc840607e1bd8ee86de0109c59422c6ddb8ea2
Thibault DEMARQUAY <thibaultdemarquay@gmail.com>
2015-01-27 11:27:18 +0100

    Generate IP added
    Classic IP generation with no verification

---
+++ src/tracethenet/generateIP.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tracethenet;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Thibault
+ */
+public class generateIP {
+    public static String generate()
+    {
+        Random r = new Random();
+        return r.nextInt(256) + "." + r.nextInt(256) + "." + r.nextInt(256) + "." + r.nextInt(256);
+    }
+}
